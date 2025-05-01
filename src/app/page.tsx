920ece8d94a20e27adc5402b7f63cb6987b6298a
Derek Gomez <63622469+GomezDerek@users.noreply.github.com>
2025-05-01 12:27:37 -0700

    sidebar added and styled!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import React from "react";
-import MainFlow from "@/components/features";
+import MainFlow from "@/components/features/MainFlow";
+import Sidebar from "@/components/features/Sidebar";
 
 import styles from "./page.module.css";
 
@@ -10,16 +11,19 @@ export default function Home() {
     <div className={styles.page}>
       <header>
         <p id="header-logo">Avantos</p>
+        <p id="slogan">AI that empowers your team to deliver services that boost loyalty and revenue</p>
       </header>
       <main>
-        <p>AI that empowers your team to deliver services that boost loyalty and revenue</p>
         
         {/* React Flow component moved to MainFlow */}
+
         <MainFlow />
+        <Sidebar />
+        
 
       </main>
-      <footer>
-      </footer>
+      {/* <footer>
+      </footer> */}
     </div>
   );
 }
--- src/components/features/MainFlow/index.tsx
+++ src/components/features/MainFlow/index.tsx
@@ -5,6 +5,7 @@ import {
   ReactFlow,
   applyNodeChanges,
   applyEdgeChanges,
+  addEdge,
   MiniMap,
   Controls,
   Background,
@@ -12,12 +13,29 @@ import {
 
 import '@xyflow/react/dist/style.css';
 
-const initialNodes = [
+// Node type alias
+type Node = {
+    id: string;
+    position: { x: number; y: number };
+    data: { label: string };
+};
+
+// Type alias for Edge objects
+type Edge = {
+  id: string;
+  source: string;
+  target: string;
+  label?: string;
+  type?: string;
+};
+
+const initialNodes: Node[] = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 100, y: 100 }, data: { label: '2' } },
 ];
 
-const initialEdges = [{ id: '1-2', source: '1', target: '2', label: 'step edge', type: 'step' }];
+// const initialEdges = [{ id: '1-2', source: '1', target: '2', label: 'step edge', type: 'step' }];
+const initialEdges: Edge[] = [];
 
 export default function MainFlow() {
   const [nodes, setNodes] = useState(initialNodes);
@@ -33,6 +51,11 @@ export default function MainFlow() {
     [],
   );
 
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
   return (
     <div id="flow-container">
       <ReactFlow
@@ -40,6 +63,7 @@ export default function MainFlow() {
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
         fitView
       >
         <Controls />
